344237070c0d445d4fc33ac475943c495e40c059
Ntofontofo <ntofontofombatha@gmail.com>
2023-12-13 15:31:02 +0200

    chap_2

---
+++ chap_2.cpp
@@ -0,0 +1,14 @@
+// This programme shows three variables defined on the same line.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// to assign a string to variable "output"
+	string Output;
+	Output="Programming is great fun";
+	cout << Output << endl;
+	
+	return 0;
+}
+ 
